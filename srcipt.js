67571e3c70fe81b1ff681f8cac5e1c83c8cb93cd
Surendra Dhaka <saabchoudhary350@gmail.com>
2024-12-22 17:20:25 +0530

    joke

---
+++ srcipt.js
@@ -0,0 +1,22 @@
+
+let get = document.querySelector("p.content");
+let button = document.querySelector("button");
+
+button.addEventListener("click",()=>{
+  button.textContent ="अगले जोक के लिए बटन दबाये"
+});
+
+button.addEventListener("click", function () {
+  fetch(
+    "https://hindi-jokes-api.onrender.com/jokes?api_key=55a0f8dd6ea42a06fd49e909c6f3"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      get.innerHTML = data.jokeContent;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
